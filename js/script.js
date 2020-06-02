4dd902fc9aa6e12519d6199238e012ebc8864c2a
AnnaCristyna <30966285+AnnaCristyna@users.noreply.github.com>
2020-06-01 22:24:13 -0300

    Add files via upload
    Modern JavaScript

--- js/script.js
+++ js/script.js
@@ -1,44 +1,35 @@
-window.addEventListener('load', start);
-var ValueRed = 127,
-  ValueGreen = 127,
-  ValueBlue = 127,
-  i,
-  ranges = [
-    document.querySelector('#rangeR'),
-    document.querySelector('#rangeG'),
-    document.querySelector('#rangeB'),
-  ];
-function start() {
+window.addEventListener('load', () => {
   firstRangeInFocus();
   console.log(ranges);
   document.addEventListener('click', wichRangeChanging);
   document.addEventListener('keydown', keypressed);
-}
+});
+let ValueRed = 127,
+  ValueGreen = 127,
+  ValueBlue = 127,
+  i;
+const ranges = [
+  document.querySelector('#rangeR'),
+  document.querySelector('#rangeG'),
+  document.querySelector('#rangeB'),
+];
 function firstRangeInFocus() {
   i = 0;
   ranges[i].focus();
 }
 
 function keypressed(event) {
-  console.log(event.key);
-  if (event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
+  if (event.keyCode === 37 || event.keyCode === 39) {
+    //ArrowLeft and ArrowRight
     wichRangeChanging(event);
   }
-  if (event.key === 'ArrowDown') {
-    if (i < 2) {
-      i++;
-    } else {
-      i = 0;
-      console.log(i);
-    }
-    console.log(i);
+  if (event.keyCode === 40) {
+    //ArrowDown
+    i < 2 ? i++ : (i = 0);
   }
-  if (event.key === 'ArrowUp') {
-    if (i > 0) {
-      i--;
-    } else {
-      i = 2;
-    }
+  if (event.keyCode === 38) {
+    //ArrowUp
+    i > 0 ? i-- : (i = 2);
   }
   ranges[i].focus();
 }
@@ -51,19 +42,16 @@ function wichRangeChanging(event) {
       inputText = document.querySelector('#valueRed');
       ValueRed = event.target.value;
       inputText.value = ValueRed;
-      //i = 1;
       break;
     case 'rangeG':
       inputText = document.querySelector('#valueGreen');
       ValueGreen = event.target.value;
       inputText.value = ValueGreen;
-      // i = 2;
       break;
     case 'rangeB':
       inputText = document.querySelector('#valueBlue');
       ValueBlue = event.target.value;
       inputText.value = ValueBlue;
-      // i = 3;
       break;
   }
   changeColor(ValueRed, ValueGreen, ValueBlue);
@@ -71,6 +59,5 @@ function wichRangeChanging(event) {
 
 function changeColor(ValueRed, ValueGreen, ValueBlue) {
   var squar = document.querySelector('#squar');
-  squar.style.backgroundColor =
-    'rgb(' + ValueRed + ',' + ValueGreen + ',' + ValueBlue + ')';
+  squar.style.backgroundColor = `rgb(${ValueRed}, ${ValueGreen}, ${ValueBlue})`;
 }
